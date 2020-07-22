53503c8b1530c0a3150f938eae25d5c242d5996b
Dev2 <dev2@gmail.com>
2020-07-22 09:32:08 +0530

    init commit

---
+++ MavenMultiModuleProj4/WishServiceMod1/src/main/java/com/murari/serviec/App.java
@@ -0,0 +1,13 @@
+package com.murari.serviec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
